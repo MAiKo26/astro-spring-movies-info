f29db628369fd017b104566e90eca2bce02bb5ce
Mohamed Aziz Karoui <medazizkaroui26@gmail.com>
2023-12-07 15:02:51 +0100

    Initial commit

---
+++ src/main/java/maiko26/movieapi/MovieService.java
@@ -0,0 +1,32 @@
+package maiko26.movieapi;
+
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getMovie(ObjectId id){
+        return movieRepository.findById(id);
+    }
+
+    public Optional<Movie> getMovieImdb(String ImdbId){
+        return movieRepository.findMovieByImdbId(ImdbId);
+    }
+
+
+
+}
+
+
