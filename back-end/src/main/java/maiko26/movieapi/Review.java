82e8848dc142f7d08a209938943f7e616d2d4f17
Mohamed Aziz Karoui <medazizkaroui26@gmail.com>
2024-12-31 13:32:49 +0100

    Update Review.java

--- back-end/src/main/java/maiko26/movieapi/Review.java
+++ back-end/src/main/java/maiko26/movieapi/Review.java
@@ -1,17 +1,11 @@
 package maiko26.movieapi;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Document(collection = "reviews")
-@Data
-@AllArgsConstructor @NoArgsConstructor
 public class Review {
     private ObjectId id;
     private String body;
@@ -22,5 +16,89 @@ public class Review {
         this.body = body;
         this.created = created;
         this.updated = updated;
+    }
+
+    public Review(ObjectId id, String body, LocalDateTime created, LocalDateTime updated) {
+        this.id = id;
+        this.body = body;
+        this.created = created;
+        this.updated = updated;
+    }
+
+    public Review() {
+    }
+
+    public ObjectId getId() {
+        return this.id;
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+
+    public LocalDateTime getCreated() {
+        return this.created;
+    }
+
+    public LocalDateTime getUpdated() {
+        return this.updated;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public void setUpdated(LocalDateTime updated) {
+        this.updated = updated;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Review)) return false;
+        final Review other = (Review) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$id = this.getId();
+        final Object other$id = other.getId();
+        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
+        final Object this$body = this.getBody();
+        final Object other$body = other.getBody();
+        if (this$body == null ? other$body != null : !this$body.equals(other$body)) return false;
+        final Object this$created = this.getCreated();
+        final Object other$created = other.getCreated();
+        if (this$created == null ? other$created != null : !this$created.equals(other$created)) return false;
+        final Object this$updated = this.getUpdated();
+        final Object other$updated = other.getUpdated();
+        if (this$updated == null ? other$updated != null : !this$updated.equals(other$updated)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Review;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $id = this.getId();
+        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
+        final Object $body = this.getBody();
+        result = result * PRIME + ($body == null ? 43 : $body.hashCode());
+        final Object $created = this.getCreated();
+        result = result * PRIME + ($created == null ? 43 : $created.hashCode());
+        final Object $updated = this.getUpdated();
+        result = result * PRIME + ($updated == null ? 43 : $updated.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Review(id=" + this.getId() + ", body=" + this.getBody() + ", created=" + this.getCreated() + ", updated=" + this.getUpdated() + ")";
     }
 }
